6fc710a75fd844c7ec092ee50b0d86c2df4b6c7d
9Prestidigitator <9prestidigitator@gmail.com>
2026-05-04 14:37:22 -0400

    Packages added

---
+++ README.md
@@ -0,0 +1,11 @@
+# reaper-flake
+
+Declare your Reaper configuration with Nix.
+
+# Packages
+
+- Reaper: 7.71
+- ReaPack: 1.2.6
+- SWS: 2.14.0.7
+
+Packages are copied from nixpkgs at the latest version with updated hashes.
---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Reaper flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs @ {
+    flake-parts,
+    nixpkgs,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+      perSystem = {system, ...}: let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in {
+        packages = rec {
+          default = reaper;
+          reaper = pkgs.callPackage ./packages/reaper.nix {};
+          reapack = pkgs.callPackage ./packages/reapack {};
+          sws = pkgs.callPackage ./packages/sws {};
+        };
+      };
+    };
+}
